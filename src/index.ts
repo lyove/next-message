bebbfb89054e8d3efe1cde16a24634ca14faa522
MetaEditor <107377084+meta-editor@users.noreply.github.com>
2023-02-04 19:47:17 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import Tooltip from "./lib";
 /**
  * Tooltip Demo
  */
+const app = document.querySelector<HTMLDivElement>("#app")!;
+app.innerHTML = `
+  <h1>Next-Tooltip</h1>
+  <a href="https://github.com/meta-explore/next-tooltip" target="_blank">Next-tooltip Github</a>
+`;
+
 const tooltip = new Tooltip();
 
 const items: HTMLCollectionOf<any> = document.getElementsByClassName("item");
