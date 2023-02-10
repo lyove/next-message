c2a33ac9f8f15c32cf862c49b772ad8a43c98636
Lyove <lyove@outlook.com>
2023-02-10 14:18:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ module.exports = defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      entry: path.resolve(__dirname, "src/index.ts"),
       name: getPackageNameCamelCase(),
       formats: ["es", "cjs", "iife"],
       fileName: (format) => fileName[format],
